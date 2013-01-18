2d38ccf971714321dd803e624a0d20939d4253d2
Hans Moog <hm@mkjc.net>
2013-01-18 02:06:42 -0500

    Added GameFolder

---
+++ riotDecode/GameFolder.php
@@ -0,0 +1,33 @@
+<?php
+namespace riotDecode;
+
+class GameFolder {
+    protected $path;
+
+	protected $riotArchiveFiles;
+
+	public function __construct($path) {
+		$this->path = preg_replace("![\\\\//]+$!", "", $path);
+	}
+
+	public function getRiotArchiveFiles() {
+		if($this->riotArchiveFiles === null) {
+			$this->riotArchiveFiles = [];
+
+			foreach(scandir($this->path . '\\RADS\\projects\\lol_game_client\\filearchives\\') as $versionFolder) {
+				if(preg_match("/([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/", $versionFolder, $matches)) {
+					foreach(scandir($this->path . '\\RADS\\projects\\lol_game_client\\filearchives\\' . $matches[1] . '\\') as $potentialRiotArchiveFile) {
+						if(preg_match("/\\.raf$/", $potentialRiotArchiveFile)) {
+							$this->riotArchiveFiles[$matches[1]] = new \riotDecode\raf\RiotArchiveFile($this->path . '\\RADS\\projects\\lol_game_client\\filearchives\\' . $matches[1] . '\\' . $potentialRiotArchiveFile);
+						}
+					}
+				}
+			}
+
+			krsort($this->riotArchiveFiles, SORT_NATURAL);
+		}
+
+		return $this->riotArchiveFiles;
+	}
+}
+?>
